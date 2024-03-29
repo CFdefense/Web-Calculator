32ee212ed9f5b770b9f3219800f3bbb78fba0606
CFdefense <152815652+CFdefense@users.noreply.github.com>
2024-03-29 15:11:58 -0400

    upd commenting

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 //Simple Calculator Using HTML, CSS and JS
 
+//retrieve the Display element from the HTML file
 const Display = document.getElementById("Display");
 
+//function to add the pressed input to the Display String
 function appendToDisplay(input) {
     Display.value += input;
 }
-
+//Function to Reset Display String
 function clearDisplay() {
     Display.value = "";
 }
 
+//Function to evaluate the string in the Calculator Display
 function calculate() {
     try {
+        //Change Displa value to the new evalutated Display
         Display.value = eval(Display.value);
     } catch(error) {
+        //If Error throw error
         Display.value = "Error";
     }
-    
 }
